6d7776cb16cb03ae93d48e701aed0061ccc1e05f
Prottay Kumar Adhikary <prottay71@gmail.com>
2026-04-21 10:43:16 +0530

    menu chage

--- assets/app.js
+++ assets/app.js
@@ -47,7 +47,7 @@ document.addEventListener('DOMContentLoaded', () => {
         userDate = initialState.targetDate;
     }
 
-    fetch('assets/menus/april26.json')
+    fetch('assets/menus/may26.json')
         .then(response => response.json())
         .then(data => {
             menuData = data;
